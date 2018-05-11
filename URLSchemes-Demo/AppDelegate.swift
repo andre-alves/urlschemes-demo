496552cdcaeb30af150197d2eb5277d53a349320
andre-alves <andre.ver93@gmail.com>
2018-05-11 16:58:29 -0300

    Start project

---
+++ URLSchemes-Demo/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  URLSchemes-Demo
+//
+//  Created by Andre Alves on 5/11/18.
+//  Copyright © 2018 Demo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ URLSchemes-Demo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  URLSchemes-Demo
+//
+//  Created by Andre Alves on 5/11/18.
+//  Copyright © 2018 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
